79ba3ade1af2e67f42293127999d8276f3a259fa
OscarGil03 <oscargil0301@gmail.com>
2023-12-01 14:06:42 -0700

    Cambios navtab

--- main.php
+++ main.php
@@ -38,7 +38,7 @@
         <a href="series.html">Series</a>
         <a href="estrenos.html">Estrenos</a>
         <a href="populares.html">Más Populares</a>
-        <a href="milista.html">Mi lista</a>
+        <a href="milista.php">Mi lista</a>
         <a href="php/logout.php" class="tableft">Cerrar Sesión
           <i class="fa fa-sign-out" aria-hidden="true"></i>
         </a>
